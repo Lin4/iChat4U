56ff1ba1696901f38ca5276d2f45d3f105c8f01e
Kalaivani Rasalingam <kalaivanirasalingam@Kalaivanis-MacBook-Pro.local>
2017-12-22 22:27:40 -0500

    setting up  outlets

--- iChat4U/Controller/CreateAccountVC.swift
+++ iChat4U/Controller/CreateAccountVC.swift
@@ -10,9 +10,18 @@ import UIKit
 
 class CreateAccountVC: UIViewController {
 
+    @IBOutlet weak var userNameTxt: UITextField!
+    @IBOutlet weak var passwordTxt: UITextField!
+    @IBOutlet weak var emailTxt: UITextField!
+    @IBOutlet weak var avatarImg: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
     
+    @IBOutlet weak var generateBackgroundColorTapped: UIButton!
+    @IBAction func chooseAvatarTapped(_ sender: Any) {
+    }
+    @IBAction func createAccountTapped(_ sender: Any) {
+    }
 }
